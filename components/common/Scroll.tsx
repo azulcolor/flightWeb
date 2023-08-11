8393df713c84113de1366019414c452bc392b067
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 <lealpastelin@gmail.com>
2023-08-11 12:32:22 -0500

    smothy scrool

--- components/common/Scroll.tsx
+++ components/common/Scroll.tsx
@@ -1,7 +1,17 @@
+'use client'
+
 export default function Scroll() {
+  const scroll = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault()
+    const section = document.querySelector('#secondSection')
+    if (!section) return
+
+    section.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
-    <span className='scroll-btn mt-16'>
-      <a href='#secondSection'>
+    <span className='scroll-btn mt-16' onClick={scroll}>
+      <a>
         <span className='mouse'>
           <span></span>
         </span>
