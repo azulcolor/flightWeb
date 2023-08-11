6315d978f9b1cf85a4d745b35a1e2e0a53eef74f
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 <lealpastelin@gmail.com>
2023-08-11 12:01:57 -0500

    new image

--- components/home/First.tsx
+++ components/home/First.tsx
@@ -4,10 +4,10 @@ import { Scroll } from '../common'
 export default function First() {
   return (
     <div className='flex'>
-      <Image src={'/images/background4.jpg'} alt='beach' width={1920} height={1080} className='w-2/3 h-[92vh]' />
+      <Image src={'/images/beach.jpg'} alt='beach' width={1920} height={1080} className='w-2/3 h-[92vh]' />
       <div className='sloganContainer'>
         <div className='slogan'>
-          <h1 className='text-background'>Bienvenido a</h1>
+          <h2 className='text-background'>Bienvenido a</h2>
           <h1 className=' text-orange-300'>FLIGHT ABSORBER</h1>
           {/* <Image src={'/images/desktopLogo.png'} alt='logo' width={400} height={100} className='shadow-white	' /> */}
           <Scroll />
